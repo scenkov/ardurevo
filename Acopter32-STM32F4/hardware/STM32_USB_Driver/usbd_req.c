e4081e87f6661bd617a4b42fdec6d3f097a053ca
emile castelnuovo <emile.castelnuovo@gmail.com>
2013-01-07 16:47:52 +0100

    Corrected a possible bug in USB drivers.

--- Acopter32-STM32F4/hardware/STM32_USB_Driver/usbd_req.c
+++ Acopter32-STM32F4/hardware/STM32_USB_Driver/usbd_req.c
@@ -824,8 +824,8 @@ void USBD_GetString(uint8_t *desc, uint8_t *unicode, uint16_t *len)
     *len =  USBD_GetLen(desc) * 2 + 2;    
     unicode[idx++] = *len;
     unicode[idx++] =  USB_DESC_TYPE_STRING;
-
-		while(*desc)
+    
+    while (*desc != NULL) 
     {
       unicode[idx++] = *desc++;
       unicode[idx++] =  0x00;
@@ -843,7 +843,7 @@ static uint8_t USBD_GetLen(uint8_t *buf)
 {
     uint8_t  len = 0;
 
-    while (*buf) 
+    while (*buf != NULL) 
     {
         len++;
         buf++;
