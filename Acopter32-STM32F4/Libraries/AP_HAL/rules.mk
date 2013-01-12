6a3e0e9caae5e9e63296558181a0fa9d38865b3d
lasernav@gmail.com <lasernav@gmail.com>
2013-01-12 17:58:54 +0100

    Add AP_BOARD_VRBRAIN and AP_HAL_VRBRAIN Driver ... Now i'm started to try to compile new classes

--- Acopter32-STM32F4/Libraries/AP_HAL/rules.mk
+++ Acopter32-STM32F4/Libraries/AP_HAL/rules.mk
@@ -11,20 +11,7 @@ CFLAGS_$(d) :=
 cSRCS_$(d) :=
 
 cppSRCS_$(d) :=
-cppSRCS_$(d) += AnalogIn.cpp
-cppSRCS_$(d) += Console.cpp
-cppSRCS_$(d) += GPIO.cpp
-cppSRCS_$(d) += HAL_VRBRAIN_Class.cpp
-cppSRCS_$(d) += I2CDriver.cpp
-cppSRCS_$(d) += PrivateMember.cpp
-cppSRCS_$(d) += RCInput.cpp
-cppSRCS_$(d) += RCOutput.cpp
-cppSRCS_$(d) += Scheduler.cpp
-cppSRCS_$(d) += Semaphores.cpp
-cppSRCS_$(d) += SPIDriver.cpp
-cppSRCS_$(d) += Storage.cpp
-cppSRCS_$(d) += UARTDriver.cpp
-cppSRCS_$(d) += Util.cpp
+#cppSRCS_$(d) += utility/Print.cpp
 
 cFILES_$(d) := $(cSRCS_$(d):%=$(d)/%)
 cppFILES_$(d) := $(cppSRCS_$(d):%=$(d)/%)
