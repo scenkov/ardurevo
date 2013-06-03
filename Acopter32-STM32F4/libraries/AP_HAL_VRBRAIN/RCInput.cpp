9bacc09fa6ffeb7cd808f91bb750fd9d2d181a07
emile castelnuovo <emile.castelnuovo@gmail.com>
2013-06-03 18:51:31 +0200

    small updates on RCinput

--- Acopter32-STM32F4/libraries/AP_HAL_VRBRAIN/RCInput.cpp
+++ Acopter32-STM32F4/libraries/AP_HAL_VRBRAIN/RCInput.cpp
@@ -16,8 +16,6 @@ extern const AP_HAL::HAL& hal;
 #define MINCHECK 900
 #define MAXCHECK 2100
 
-
-
 // STANDARD PPM VARIABLE
 
 volatile uint16_t rcPinValue[8] =
@@ -25,7 +23,6 @@ volatile uint16_t rcPinValue[8] =
     1500, 1500, 1000, 1500, 1500, 1500, 1500, 1500
     };
 // interval [1000;2000]
-
 
 // ***PPM SUM SIGNAL***
 static uint8_t rcChannel[12];
@@ -98,14 +95,13 @@ typedef struct
     } tPinTimingData;
 volatile static tPinTimingData pinData[8];
 
-
 void rxIntPPMSUM(void)
     {
     volatile unsigned int now;
     volatile unsigned int diff;
     int i;
 
-    hal.scheduler->suspend_timer_procs();
+    //hal.scheduler->suspend_timer_procs();
 
     now = hal.scheduler->micros();
     diff = now - last;
@@ -143,9 +139,8 @@ void rxIntPPMSUM(void)
 	radio_status_rc = 0;
 	}
 
-    hal.scheduler->resume_timer_procs();
+    //hal.scheduler->resume_timer_procs();
     }
-
 
 /*
  0 PE9		75	PWM_IN0		 IRQ 5-9  * Conflict  PPM1
@@ -159,178 +154,192 @@ void rxIntPPMSUM(void)
  */
 
 // Constructors ////////////////////////////////////////////////////////////////
-
 using namespace VRBRAIN;
 
-	/* ADD ON PIN NORMALLY AVAILABLE ON RX BUT IF PPM SUM ACTIVE AVAILABLE AS SERVO OUTPUT */
+/* ADD ON PIN NORMALLY AVAILABLE ON RX BUT IF PPM SUM ACTIVE AVAILABLE AS SERVO OUTPUT */
 
 void VRBRAINRCInput::InitDefaultPPMSUM(char board)
-	    {
-	    switch (board)
-		{
-	    case 10:
-		ppm_sum_channel = 75;
-		rcChannel[0] = 0;
-		rcChannel[1] = 1;
-		rcChannel[2] = 2;
-		rcChannel[3] = 3;
-		rcChannel[4] = 4;
-		rcChannel[5] = 5;
-		rcChannel[6] = 6;
-		rcChannel[7] = 7;
-		rcChannel[8] = 8;
-		break;
+    {
+    switch (board)
+	{
+    case 10:
+	ppm_sum_channel = 75;
+	rcChannel[0] = 0;
+	rcChannel[1] = 1;
+	rcChannel[2] = 2;
+	rcChannel[3] = 3;
+	rcChannel[4] = 4;
+	rcChannel[5] = 5;
+	rcChannel[6] = 6;
+	rcChannel[7] = 7;
+	rcChannel[8] = 8;
+	break;
 
-	    case 11:
-		ppm_sum_channel = 75;
-		rcChannel[0] = 0;
-		rcChannel[1] = 1;
-		rcChannel[2] = 2;
-		rcChannel[3] = 3;
-		rcChannel[4] = 4;
-		rcChannel[5] = 5;
-		rcChannel[6] = 6;
-		rcChannel[7] = 7;
-		rcChannel[8] = 8;
+    case 11:
+	ppm_sum_channel = 75;
+	rcChannel[0] = 0;
+	rcChannel[1] = 1;
+	rcChannel[2] = 2;
+	rcChannel[3] = 3;
+	rcChannel[4] = 4;
+	rcChannel[5] = 5;
+	rcChannel[6] = 6;
+	rcChannel[7] = 7;
+	rcChannel[8] = 8;
 
-		break;
-		}
-	    }
+	break;
+	}
+    }
 
 void VRBRAINRCInput::InitPPM(void)
-	    {
-	        for(byte channel = 0; channel < 8; channel++)
-    		   pinData[channel].edge = FALLING_EDGE;
+    {
+    for (byte channel = 0; channel < 8; channel++)
+	pinData[channel].edge = FALLING_EDGE;
 
-    	//attachPWMCaptureCallback(PWMCaptureCallback);
-    	pwmInit();
-}
+    //attachPWMCaptureCallback(PWMCaptureCallback);
+    pwmInit();
+    }
 
 void VRBRAINRCInput::InitDefaultPPM(char board)
-	    {
-	    switch (board)
-		{
-	    case 0:
+    {
+    switch (board)
+	{
+    case 0:
 
-		// MP32V1F1
-		input_channel_ch1 = 22;
-		input_channel_ch2 = 23;
-		input_channel_ch3 = 24;
-		input_channel_ch4 = 89;
-		input_channel_ch5 = 59;
-		input_channel_ch6 = 62;
-		input_channel_ch7 = 60;
-		input_channel_ch8 = 0;
-		break;
-	    case 1:
+	// MP32V1F1
+	input_channel_ch1 = 22;
+	input_channel_ch2 = 23;
+	input_channel_ch3 = 24;
+	input_channel_ch4 = 89;
+	input_channel_ch5 = 59;
+	input_channel_ch6 = 62;
+	input_channel_ch7 = 60;
+	input_channel_ch8 = 0;
+	break;
+    case 1:
 
-		// MP32V3F3
-		input_channel_ch1 = 22;
-		input_channel_ch2 = 63;
-		input_channel_ch3 = 66;
-		input_channel_ch4 = 89;
-		input_channel_ch5 = 59;
-		input_channel_ch6 = 62;
-		input_channel_ch8 = 12;
-		input_channel_ch7 = 60;
-		/*
-		 input_channel_ch7=12;
-		 input_channel_ch8=60;
-		 */
+	// MP32V3F1
+	input_channel_ch1 = 22;
+	input_channel_ch2 = 63;
+	input_channel_ch3 = 66;
+	input_channel_ch4 = 89;
+	input_channel_ch5 = 59;
+	input_channel_ch6 = 62;
+	input_channel_ch7 = 60;
+	input_channel_ch8 = 12;
+	/*
+	 input_channel_ch7=12;
+	 input_channel_ch8=60;
+	 */
 
-		break;
-	    case 2:
+	break;
+    case 2:
 
-		// VRBRAIN
-		//PIN 13 freeze board (was USB DISC)
-		/*
-		 PE9		75	PWM_IN0		IRQ 5-9  * Conflict  PPM1
-		 PE11		80	PWM_IN1		IRQ 10-15			  PPM2
-		 PE13		86	PWM_IN2		IRQ 10-15			  PPM3
-		 PE14		89	PWM_IN3		IRQ 10-15			  PPM4
-		 PC6		12	PWM_IN4		IRQ 5-9			  PPM5
-		 PC7		13	PWM_IN5		IRQ 5-9			  PPM6
-		 PC8		14	PWM_IN6		IRQ 5-9			  PPM7
-		 PC9		15	PWM_IN7	     	IRQ 5-9   * Conflict (PPMSUM)
-		 */
+	// VRBRAIN
+	//PIN 13 freeze board (was USB DISC)
+	/*
+	 PE9		75	PWM_IN0		IRQ 5-9  * Conflict  PPM1
+	 PE11		80	PWM_IN1		IRQ 10-15			  PPM2
+	 PE13		86	PWM_IN2		IRQ 10-15			  PPM3
+	 PE14		89	PWM_IN3		IRQ 10-15			  PPM4
+	 PC6		12	PWM_IN4		IRQ 5-9			  PPM5
+	 PC7		13	PWM_IN5		IRQ 5-9			  PPM6
+	 PC8		14	PWM_IN6		IRQ 5-9			  PPM7
+	 PC9		15	PWM_IN7	     	IRQ 5-9   * Conflict (PPMSUM)
+	 */
 
-		input_channel_ch1 = 75;
-		input_channel_ch2 = 80;
-		input_channel_ch3 = 86;
-		input_channel_ch4 = 89;
-		input_channel_ch5 = 12;
-		input_channel_ch6 = 13;
-		input_channel_ch7 = 14;
-		input_channel_ch8 = 0;
+	input_channel_ch1 = 75;
+	input_channel_ch2 = 80;
+	input_channel_ch3 = 86;
+	input_channel_ch4 = 89;
+	input_channel_ch5 = 12;
+	input_channel_ch6 = 13;
+	input_channel_ch7 = 14;
+	input_channel_ch8 = 0;
 
-		//input_channel_ch8=15;
-		break;
+	//input_channel_ch8=15;
+	break;
 
-		}
-}
+	}
+    }
 
 // Public Methods //////////////////////////////////////////////////////////////
 void VRBRAINRCInput::InitPPMSUM(void)
-	    {
-	    hal.gpio->pinMode(ppm_sum_channel, INPUT);
-	    hal.gpio->attach_interrupt(ppm_sum_channel, rxIntPPMSUM, RISING);
-	    }
+    {
+    hal.gpio->pinMode(ppm_sum_channel, INPUT);
+    hal.gpio->attach_interrupt(ppm_sum_channel, rxIntPPMSUM, RISING);
+    }
 
 uint16_t VRBRAINRCInput::InputCh(unsigned char ch)
-	    {
-	    uint16_t data;
-	    noInterrupts();
-	    if (_iboard < 10)
-		data = pwmRead(ch);
-	    else
-		{
-		data = rcValue[rcChannel[ch + 1]];
-		}
-	    interrupts();
-	    return data; // We return the value correctly copied when the IRQ's where disabled
-	    }
+    {
+    uint16_t data;
+    noInterrupts();
+    if (_iboard < 10)
+	data = pwmRead(ch);
+    else
+	{
+	data = rcValue[rcChannel[ch + 1]];
+	}
+    interrupts();
+    return data; // We return the value correctly copied when the IRQ's where disabled
+    }
 
 unsigned char VRBRAINRCInput::GetState(void)
-	    {
-	    return (radio_status_rc);
-	    }
+    {
+    return (radio_status_rc);
+    }
 
 VRBRAINRCInput::VRBRAINRCInput()
-	    {
-	    }
+    {
+    }
 
 void VRBRAINRCInput::init(void* machtnichts)
-	    {
+    {
+
+    /*initial check for pin2-pin3 bridge. If detected switch to PPMSUM  */
+    //default to standard PPM
     _iboard = 2;
 
     uint8_t channel3_status = 0;
     uint8_t pin2, pin3;
+    //input pin 2
     pin2 = 80;
+    //input pin 3
     pin3 = 86;
+
+    //set pin2 as output and pin 3 as input
     hal.gpio->pinMode(pin2, OUTPUT);
     hal.gpio->pinMode(pin3, INPUT);
 
-    hal.gpio->write(pin3,0);
-
+    //default pin3 to 0
+    hal.gpio->write(pin3, 0);
     hal.scheduler->delay(1);
 
-    hal.gpio->write(pin2,1);
-
+    //write 1 to pin 2 and read pin3
+    hal.gpio->write(pin2, 1);
     hal.scheduler->delay(1);
+    //if pin3 is 1 increment counter
+    if (hal.gpio->read(pin3) == 1)
+	channel3_status++;
 
-    if(hal.gpio->read(pin3) == 1) channel3_status++;
-    hal.gpio->write(pin2,0);
-
+    //write 0 to pin 2 and read pin3
+    hal.gpio->write(pin2, 0);
     hal.scheduler->delay(1);
+    //if pin3 is 0 increment counter
+    if (hal.gpio->read(pin3) == 0)
+	channel3_status++;
 
-    if(hal.gpio->read(pin3) == 0) channel3_status++;
-    hal.gpio->write(pin2,1);
-
+    //write 1 to pin 2 and read pin3
+    hal.gpio->write(pin2, 1);
     hal.scheduler->delay(1);
+    //if pin3 is 1 increment counter
+    if (hal.gpio->read(pin3) == 1)
+	channel3_status++;
 
-    if(hal.gpio->read(pin3) == 1) channel3_status++;
+    //if counter is 3 then we are in PPMSUM
+    if (channel3_status == 3)
+	_iboard = 11;
 
-    if(channel3_status == 3) _iboard = 11;
     if (_iboard < 10)
 	{
 	// Init Radio In
@@ -342,60 +351,60 @@ void VRBRAINRCInput::init(void* machtnichts)
 	// Init Radio In
 	hal.console->println("Init Default PPMSUM");
 	InitDefaultPPMSUM(_iboard);
-	hal.console->println("Init PPMSUM HWD");
 	InitPPMSUM();
 	}
-}
+    }
 
 uint8_t VRBRAINRCInput::valid_channels()
-	    {
-	    return 1;
-	    }
+    {
+    return 1;
+    }
 
 uint16_t VRBRAINRCInput::read(uint8_t ch)
-	    {
-	    uint16_t data;
-	    noInterrupts();
-	    if (_iboard < 10){
-		//data = rcPinValue[ch];
-		data = pwmRead(ch);
-	    }
-	    else
-		{
-		data = rcValue[rcChannel[ch + 1]];
-		}
-	    interrupts();
-	    return data; // We return the value correctly copied when the IRQ's where disabled
-	    }
+    {
+    uint16_t data;
+    noInterrupts();
+    if (_iboard < 10)
+	{
+	//data = rcPinValue[ch];
+	data = pwmRead(ch);
+	}
+    else
+	{
+	data = rcValue[rcChannel[ch + 1]];
+	}
+    interrupts();
+    return data; // We return the value correctly copied when the IRQ's where disabled
+    }
 
-	uint8_t VRBRAINRCInput::read(uint16_t* periods, uint8_t len)
+uint8_t VRBRAINRCInput::read(uint16_t* periods, uint8_t len)
+    {
+    noInterrupts();
+    for (uint8_t i = 0; i < len; i++)
+	{
+	if (_iboard < 10)
+	    //periods[i] = rcPinValue[i];
+	    periods[i] = pwmRead(i);
+	else
 	    {
-	    noInterrupts();
-	    for (uint8_t i = 0; i < len; i++)
-		{
-		if (_iboard < 10)
-		    //periods[i] = rcPinValue[i];
-		    periods[i] = pwmRead(i);
-		else
-		    {
-		    periods[i] = rcValue[rcChannel[i + 1]];
-		    }
-		}
-	    interrupts();
-	    return len;
+	    periods[i] = rcValue[rcChannel[i + 1]];
 	    }
+	}
+    interrupts();
+    return len;
+    }
 
-	bool VRBRAINRCInput::set_overrides(int16_t *overrides, uint8_t len)
-	    {
-	    return true;
-	    }
+bool VRBRAINRCInput::set_overrides(int16_t *overrides, uint8_t len)
+    {
+    return true;
+    }
 
-	bool VRBRAINRCInput::set_override(uint8_t channel, int16_t override)
-	    {
-	    return true;
-	    }
+bool VRBRAINRCInput::set_override(uint8_t channel, int16_t override)
+    {
+    return true;
+    }
 
-	void VRBRAINRCInput::clear_overrides()
-	    {
-	    }
+void VRBRAINRCInput::clear_overrides()
+    {
+    }
 
