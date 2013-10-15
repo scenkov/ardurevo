171c31b0aadccab69b6e18690dd49550054062d0
Emile Castelnuovo <emile.castelnuovo@gmail.com>
2013-10-15 22:48:09 +0200

    [WIP] master_latest_rc4: corrected infinite loop in EEPROM read.

--- Acopter32-STM32F4/libraries/AP_HAL_VRBRAIN/Storage.cpp
+++ Acopter32-STM32F4/libraries/AP_HAL_VRBRAIN/Storage.cpp
@@ -54,11 +54,21 @@ void VRBRAINStorage::read_block(void* dst, uint16_t src, size_t n) {
 	//sEE_WaitEepromStandbyState();
 
 	uint32_t ret = sEE_ReadBuffer(buff, src, &numbytes);
+
 	if(ret == 1){
-	    //hal.console->println_P("i2c timeout read block");
-		return;
+	    hal.gpio->write(20, 1);
+	    return;
 	}
-	while(numbytes > 0);
+
+	uint32_t time =  ((uint32_t)(10 * 0x1000));
+	while(numbytes > 0){
+	    if ((time--) == 0)
+		{
+		hal.gpio->write(20, 1);
+		return;
+		}
+	}
+
 }
 
 void VRBRAINStorage::write_block(uint16_t dst,const void* src, size_t n)
